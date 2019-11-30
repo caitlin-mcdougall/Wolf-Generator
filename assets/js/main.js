2efe0d3a34f7812d35c823a207d5216a5b9eb196
caitlin-mcdougall <41774257+caitlin-mcdougall@users.noreply.github.com>
2019-11-30 15:09:33 +0000

    Add core functionality to site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
-var app = new Vue({
+import fetch from 'node-fetch';
+global.fetch = fetch;
+var greeting = new Vue({
     el: "#greeting",
     data:{
-        message: "Hello World!"
+        message: "Welcome to your daily dose of adorably majestic content"
+    }
+})
+
+var action = new Vue({
+    el: "#action",
+    data:{
+        image: "./images/initial.jpg"
+        
+    },
+    methods:{
+        changeImage: function(){
+        this.image="./images/mountain.jpg"
+        }
     }
 })
